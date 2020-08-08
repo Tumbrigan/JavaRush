3bd77f4327887a3fbb1b1d1f78598daf69b25b4b
Ihor Kucher <ihor.kucher.94@gmail.com>
2020-08-08 15:30:23 +0300

    add Restaurant (3)

--- 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
+++ 3.JavaMultithreading/src/com/javarush/task/task27/task2712/Tablet.java
@@ -3,9 +3,10 @@ package com.javarush.task.task27.task2712;
 import com.javarush.task.task27.task2712.kitchen.Order;
 
 import java.io.IOException;
+import java.util.Observable;
 import java.util.logging.Logger;
 
-public class Tablet {
+public class Tablet extends Observable {
     private final int number;
     private static Logger logger = Logger.getLogger(Tablet.class.getName());
 
@@ -13,12 +14,18 @@ public class Tablet {
         this.number = number;
     }
 
-    public void createOrder() {
+    public Order createOrder() {
         try {
             Order order = new Order(this);
+            setChanged();
+            notifyObservers(order);
+            if (!order.getDishes().isEmpty()) {
+                return order;
+            }
         } catch (IOException e) {
             logger.severe("Console is unavailable.");
         }
+        return null;
     }
 
     public int getNumber() {
