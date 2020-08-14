dcf495978dcf2be943e7a410fd015ce701c72e1a
Ihor Kucher <ihor.kucher.94@gmail.com>
2020-08-14 10:30:57 +0300

    2048 (5)

--- 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Model.java
@@ -1,18 +1,40 @@
 package com.javarush.task.task35.task3513;
 
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
 public class Model {
     private static final int FIELD_WIDTH = 4;
     private Tile[][] gameTiles;
 
     public Model() {
         gameTiles = new Tile[FIELD_WIDTH][FIELD_WIDTH];
+        resetGameTiles();
 
+
+    }
+
+    void resetGameTiles() {
         for (int i = 0; i < FIELD_WIDTH; i++) {
             for (int j = 0; j < FIELD_WIDTH; j++) {
                 gameTiles[i][j] = new Tile();
             }
         }
+        addTile();
+        addTile();
     }
 
+    private void addTile() {
+        List<Tile> emptyTiles = getEmptyTiles();
+        if (emptyTiles.isEmpty()) return;
+        int randomInt =  (int) (Math.random() * emptyTiles.size());;
+        emptyTiles.get(randomInt).value = Math.random() < 0.9 ? 2 : 4;
+    }
 
+    private List<Tile> getEmptyTiles() {
+        return Arrays.stream(gameTiles).flatMap(Arrays::stream)
+                .filter(Tile::isEmpty).collect(Collectors.toList());
+    }
 }
---
+++ 3.JavaMultithreading/src/com/javarush/task/task35/task3513/Test.java
@@ -0,0 +1,14 @@
+package com.javarush.task.task35.task3513;
+
+import java.util.Random;
+
+public class Test {
+    public static void main(String[] args) {
+        for (int i = 0; i < 100; i++) {
+            int randomInt = new Random().nextInt(50);
+            System.out.println(randomInt);
+        }
+
+
+    }
+}
